0c94053caafbf90edec9b2215de8a1a4c251c566
mamtrix <46560245+mamtrix@users.noreply.github.com>
2019-01-11 06:12:16 -0200

    Add files via upload

---
+++ led_blink_git.ino
@@ -0,0 +1,17 @@
+//  Arduino test for git
+//  by mamtrix
+//  11/01/2019
+
+void setup() {
+
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(1000);
+  
+}
